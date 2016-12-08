59227b19556d056470589f1d962d383e45b03b8b
oelderinkX <joelderink.wale@gmail.com>
2016-12-08 21:43:39 +1300

    common

--- script/common.js
+++ script/common.js
@@ -1,3 +1,7 @@
+const url = require('url')
+const params = url.parse(process.env.DATABASE_URL);
+const auth = params.auth.split(':');
+
 function guid() {
   return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
     s4() + '-' + s4() + s4() + s4();
@@ -7,4 +11,17 @@ function s4() {
   return Math.floor((1 + Math.random()) * 0x10000)
     .toString(16)
     .substring(1);
+}
+
+function postgresConfig() {
+  var config = {
+    user: auth[0],
+    password: auth[1],
+    host: params.hostname,
+    port: params.port,
+    database: params.pathname.split('/')[1],
+    ssl: true	  
+  };
+  
+  return config;
 }
