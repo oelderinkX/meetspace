0407bbb525fdc68a3ee8dda195d41aa13220f62d
oelderinkX <joelderink.wale@gmail.com>
2016-12-08 14:52:19 +1300

    logout

--- logout.js
+++ logout.js
@@ -1,16 +1,20 @@
-var mysql = require('mysql');
+var pg = require('pg');
 var bodyParser = require('body-parser');
 var fs = require("fs");
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-var pool = mysql.createPool({
-  connectionLimit : 10,
-  host     : 'localhost',
-  user     : 'root',
-  password : 'oelderink',
-  database : 'meetspace'
-});
+var config = {
+  host: 'ec2-54-235-111-59.compute-1.amazonaws.com',
+  user: 'qpidruggfishtd',
+  database: 'df8uavpng011op',
+  password: '2RgFbHtlj9eQO8MRwP48Vi_NFV',
+  port: 5432,
+  max: 10,
+  idleTimeoutMillis: 30000
+};
+
+var pool = new pg.Pool(config);
 
 var registrationPage = fs.readFileSync(__dirname + "/webpage/registration.html", "utf8");
 
@@ -19,18 +23,20 @@ module.exports = function(app){
 	app.all('/logout', urlencodedParser, function(req, res) {
 		
 		var registrationStatus = 'OK';
-		var sql = 'DELETE FROM session WHERE email = ? AND sessionId = ?;';
+		var sql = 'DELETE FROM meetspace.session WHERE email = ? AND sessionId = ?;';
 		
 		var email = req.cookies['email'];
 		var sessionId = req.cookies['sessionId'];
 		
-		pool.getConnection(function(err, connection) {
-			connection.query(sql, [ email, sessionId], function(err, row) {
-				connection.release();
+		pool.connect(function(err, connection, done) {
+			connection.query(sql, [ email, sessionId], function(err, result) {
+				done()
 				
 				if (err) {
 					console.error(err);
 				}
+				
+				console.log(result);
 				
 				res.clearCookie('email');
 				res.clearCookie('sessionId');
