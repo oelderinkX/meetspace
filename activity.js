4b8e885d619cf2d8d30f5462c0642ead56d356e2
oelderinkX <joelderink.wale@gmail.com>
2016-12-21 22:00:26 +1300

    activity

--- activity.js
+++ activity.js
@@ -44,9 +44,112 @@ function getDay(day) {
 	}
 }
 
+function renderPage(country, region, city, game, res) {
+	var details = details = '<html><body>';
+	
+	var sql = 'SELECT activityId, title, game, city, region, country, time, day, description FROM meetspace.activity';
+	
+	sql = addWhereClause(sql, 'country', country);
+	sql = addWhereClause(sql, 'region', region);
+	sql = addWhereClause(sql, 'city', city);
+	sql = addWhereClause(sql, 'game', game);
+	
+	sql += ' LIMIT 10';
+	
+	pool.connect(function(err, client, done) {
+		
+		if(err) {
+			console.log("ERROR! " + err)
+		}
+		
+		client.query(sql, function(err, result) {
+			done();
+			
+			if (err) {
+				console.log("errors!");
+			} else {
+				if (result.rows.length == 1) {
+					var title = result.rows[0].title;
+					var time = result.rows[0].time;
+					var day = result.rows[0].day;
+					var activityId = result.rows[0].activityId;
+					
+					game = result.rows[0].game;
+					description = result.rows[0].description;
+					details += '<h2>' + title + ', ' + getDay(day) + ' ' + getTime(time) + '</h2>';
+					details += '' + description + '';
+					
+					
+					var whosgoingsql = "SELECT user.username FROM meetspace.whosgoing JOIN meetspace.user ON whosgoing.userId = user.id WHERE whosgoing.activityId = ?"
+					pool.connect(function(err, client, done) {
+						client.query(whosgoingsql, [activityId], function(err, result) {
+							done();
+							
+							details += '<br/><br/>Whos going:<br/><br/>';
+							
+							if (!result) {
+								details += 'Nobody';
+							} else {
+								for (var i = 0; i < result.rows.length; i++) {
+									var username = result.rows[i].username;
+									details +=  username + '<br/>';
+								}
+							}
+
+							details += 'action: ' + action;
+							
+							details += '</body></html>';
+							res.send(details);
+						});
+					});
+					
+				} else if (result.rows.length == 0) {
+					//display 'would you like to create'
+					details += 'no activty for ' + game + '.  Would you like to create it?';
+					details += '</body></html>';
+					res.send(details);
+				} else if (result.rows.length > 1) {
+					
+					details += 'Activities in your area:<br/><br/>';
+					
+					for (var i = 0; i < result.rows.length; i++) {
+						var title = result.rows[i].title
+						game = result.rows[i].game;
+						city = result.rows[i].city;
+						region = result.rows[i].region;
+						country = result.rows[i].country;
+						description = result.rows[i].description;
+						
+						var link = 'https://meetspacev1.herokuapp.com/';
+						if (region) {
+							link += country + '/' + region + '/' + city + '/' + game;
+						} else {
+							link += country + '/' + city + '/' + game;
+						}
+						
+						details += title + ' <a href="' + link + '">' + link + '</a><br/>';
+					}
+					
+					details += '</body></html>';
+					res.send(details);
+				}
+			}
+		});
+	});
+}
+
+function performAction(country, region, city, game, action, res) {
+	if (action) {
+		console.log('do action');
+		renderPage(country, region, city, game, res);
+	} else {
+		console.log('do NOT action');
+		renderPage(country, region, city, game, res);
+	}
+}
+
 module.exports = function(app) {
     app.get('*', function(req, res) {
-	//app.get('*', urlencodedParser, function(req, res) {
 		var action = req.param('action');
 		
 		var url = req.url;
@@ -61,122 +164,20 @@ module.exports = function(app) {
 		var region = '';
 		var game = '';
 		
-		var details = details = '<html><body>';
-		
 		if(params.length === 4) {
 			country = params[1];
 			city = params[2];
 			game = params[3];
-			
-			//details += 'Country: ' + country + '<br/>';
-			//details += 'City: ' + city  + '<br/>';
-			//details += 'Game: ' + game  + '<br/>';
 		} else if (params.length === 5) {
 			country = params[1];
 			region = params[2];
 			city = params[3];
 			game = params[4];
-			
-			//details += 'Country: ' + country + '<br/>';
-			//details += 'Region: ' + region  + '<br/>';
-			//details += 'City: ' + city  + '<br/>';
-			//details += 'Game: ' + game  + '<br/>';
-			//details += '<br/>';
 		} else {
 			//details = 'unknown activity';
 			//res.send(details);
 		}
 		
-		var sql = 'SELECT activityId, title, game, city, region, country, time, day, description FROM meetspace.activity';
-		
-		sql = addWhereClause(sql, 'country', country);
-		sql = addWhereClause(sql, 'region', region);
-		sql = addWhereClause(sql, 'city', city);
-		sql = addWhereClause(sql, 'game', game);
-		
-		sql += ' LIMIT 10';
-		
-		console.log(sql);
-		
-		pool.connect(function(err, client, done) {
-			
-			if(err) {
-				console.log("ERROR! " + err)
-			}
-			
-			client.query(sql, function(err, result) {
-				done();
-				
-				if (err) {
-					console.log("errors!");
-				} else {
-					if (result.rows.length == 1) {
-						var title = result.rows[0].title;
-						var time = result.rows[0].time;
-						var day = result.rows[0].day;
-						var activityId = result.rows[0].activityId;
-						
-						game = result.rows[0].game;
-						description = result.rows[0].description;
-						details += '<h2>' + title + ', ' + getDay(day) + ' ' + getTime(time) + '</h2>';
-						details += '' + description + '';
-						
-						
-						var whosgoingsql = "SELECT user.username FROM meetspace.whosgoing JOIN meetspace.user ON whosgoing.userId = user.id WHERE whosgoing.activityId = ?"
-						pool.connect(function(err, client, done) {
-							client.query(whosgoingsql, [activityId], function(err, result) {
-								done();
-								
-								details += '<br/><br/>Whos going:<br/><br/>';
-								
-								if (!result) {
-									details += 'Nobody';
-								} else {
-									for (var i = 0; i < result.rows.length; i++) {
-										var username = result.rows[i].username;
-										details +=  username + '<br/>';
-									}
-								}
-
-								details += 'action: ' + action;
-								
-								details += '</body></html>';
-								res.send(details);
-							});
-						});
-						
-					} else if (result.rows.length == 0) {
-						//display 'would you like to create'
-						details += 'no activty for ' + game + '.  Would you like to create it?';
-						details += '</body></html>';
-						res.send(details);
-					} else if (result.rows.length > 1) {
-						
-						details += 'Activities in your area:<br/><br/>';
-						
-						for (var i = 0; i < result.rows.length; i++) {
-							var title = result.rows[i].title
-							game = result.rows[i].game;
-							city = result.rows[i].city;
-							region = result.rows[i].region;
-							country = result.rows[i].country;
-							description = result.rows[i].description;
-							
-							var link = 'https://meetspacev1.herokuapp.com/';
-							if (region) {
-								link += country + '/' + region + '/' + city + '/' + game;
-							} else {
-								link += country + '/' + city + '/' + game;
-							}
-							
-							details += title + ' <a href="' + link + '">' + link + '</a><br/>';
-						}
-						
-						details += '</body></html>';
-						res.send(details);
-					}
-				}
-			});
-		});
+		performAction(country, region, city, game, action, res);
     });
 }
