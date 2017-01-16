c78811cb0c42ba3b946cb42958b6f066fa1327b3
oelderinkX <joelderink.wale@gmail.com>
2017-01-16 21:40:31 +1300

    loginredirect

--- login.js
+++ login.js
@@ -63,7 +63,7 @@ module.exports = function(app){
 						res.cookie('username', result.rows[0].ret_username);
 						res.cookie('email' , email);
 						res.cookie('sessionId' , sessionId);
-						res.send('<html><body>successful? ' + 'hello' + '</body></html>');
+						//res.send('<html><body>successful? ' + 'hello' + '</body></html>');
 						
 						var backUrl = req.header('Referer') || '/';
 						res.redirect(backUrl);
