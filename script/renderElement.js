a4efcc01fd45b2873f7099af4b17de6588bd68eb
oelderinkX <joelderink.wale@gmail.com>
2017-02-16 20:46:06 +1300

    new render script

---
+++ script/renderElement.js
@@ -0,0 +1,30 @@
+function getTime(time) {
+	var strTime = time;
+	return strTime;
+}
+
+function getDay(day) {
+	if (day == 0) {
+		return "Sunday";
+	} else if (day == 1) {
+		return "Monday";
+	} else if (day == 2) {
+		return "Tuesday";
+	} else if (day == 3) {
+		return "Wednesday";
+	} else if (day == 4) {
+		return "Thursday";
+	} else if (day == 5) {
+		return "Friday";
+	} else {
+		return "Saturday";
+	}
+}
+
+function activityTitle(webpage, title, day, time) {
+  
+  webpage.replace('!%TITLE%!', title + ', ' + getDay(day) + ' ' + getTime(time));
+  
+  return webpage;
+}
+module.exports.activityTitle = activityTitle;
