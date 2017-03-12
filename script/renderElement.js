72aa0fd3feaf879d8dc16e31c1c084b2d7347965
oelderinkX <joelderink.wale@gmail.com>
2017-03-12 14:29:42 +1300

    render

--- script/renderElement.js
+++ script/renderElement.js
@@ -142,3 +142,18 @@ function error(webpage, error) {
 	return webpage;
 }
 module.exports.error = error;
+
+function breadcrumb(webpage, country, region, city, game) {
+	var breadcrumbElement = '<a href="/">Home</a>';
+	breadcrumbElement += '<a href="/' + country + '">' + country + '</a>';
+	
+	if (region) {
+		breadcrumbElement += '<a href="/' + region + '">' + region + '</a> /';
+		breadcrumbElement += '<a href="/' + country + '/' + region '/' city + '">' + city + '</a> /' ;
+		breadcrumbElement += '<a href="/' + country + '/' + region + '/' city + '/' + game + '">' + game + '</a> /' ;
+	} else {
+		breadcrumbElement += '<a href="/' + country + '/' city + '">' + city + '</a> /' ;
+		breadcrumbElement += '<a href="/' + country + '/' city + '/' + game + '">' + game + '</a> /' ;
+	}
+}
+module.exports.breadcrumb = breadcrumb;
