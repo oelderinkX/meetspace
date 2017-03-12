f885733e9f4b96627626970af136bdf62489850b
oelderinkX <joelderink.wale@gmail.com>
2017-03-12 13:35:47 +1300

    activitydetails

--- activity.js
+++ activity.js
@@ -13,20 +13,6 @@ var style1Page = fs.readFileSync(__dirname + "/webpage/activity_style1.html", "u
 var listPage = fs.readFileSync(__dirname + "/webpage/activity_list.html", "utf8");
 var infoPage = fs.readFileSync(__dirname + "/webpage/infopage.html", "utf8");
 
-function addWhereClause(sql, fieldName, fieldValue) {
-	var newSql = sql;
-	
-	if(fieldValue) {
-		if (newSql.includes("WHERE")) {
-			newSql += ' AND ' + fieldName + '= \'' + fieldValue + '\'';
-		} else {
-			newSql += ' WHERE ' + fieldName + '= \'' + fieldValue + '\'';
-		}
-	}
-	
-	return newSql;
-}
-
 function getUrl(country, region, city, game) {
 	var url = common.webpage_url;
 	
@@ -52,19 +38,12 @@ function renderPage(country, region, city, game, req, res) {
 	
 	webpage = renderElement.login(webpage, username, common.webpage_url);
 	
-	//var sql = 'SELECT activityId, title, game, city, region, country, time, day, styleid, description FROM meetspace.activity';
-	
 	var sql = "SELECT * FROM meetspace.find_activity($1, $2, $3, $4);"
-	
-	//sql = addWhereClause(sql, 'country', country);
-	//sql = addWhereClause(sql, 'region', region);
-	//sql = addWhereClause(sql, 'city', city);
-	//sql = addWhereClause(sql, 'game', game);
-	
+
 	pool.connect(function(err, client, done) {
 		
 		if(err) {
-			console.log("ERROR! " + err)
+			console.log("ERROR! " + err);
 		}
 		
 		client.query(sql, [country, region, city, game], function(err, result) {
@@ -74,25 +53,13 @@ function renderPage(country, region, city, game, req, res) {
 				console.log("errors!");
 			} else {
 				if (result.rows.length == 1) {
-					
-					console.log(result);
-
-					var activityId = result.rows[0].ret_activityid;					
-					var title = result.rows[0].ret_title;
-					var time = result.rows[0].ret_time;
-					var day = result.rows[0].ret_day;
-					
 					var showjoin = 'none';
 					var showunjoin = 'none';
 					var showattend = 'none';
 					var showunattend = 'none';
 					var showreset = 'none';
-					
 					var showpost = 'none'; 
 					var showinvite = 'none'; 
-					
-					game = result.rows[0].ret_game;
-					description = result.rows[0].ret_description;
 					var actionlink = '/' + country;
 					if (region) {
 						actionlink += '/' + region;
@@ -100,86 +67,109 @@ function renderPage(country, region, city, game, req, res) {
 					actionlink += '/' + city;
 					actionlink += '/' + game;
 					
-					if (username && sessionId) {
-						showjoin = 'inline';
-						showunjoin = 'inline';
-						showattend = 'inline';
-						showunattend = 'inline';
-						showreset = 'inline';
-						showpost = 'inline';
-						showinvite = 'inline';
-					}
-					
-					webpage = renderElement.activityTitle(webpage, title);
-					webpage = renderElement.activityTime(webpage, day, time);
-					webpage = webpage.replace('!%DESCRIPTION%!', description);
-					webpage = common.replaceAll(webpage, '!%ACTION%!', actionlink);
-					webpage = common.replaceAll(webpage, '!%ACTIVITYID%!', activityId);
-										
-					webpage = common.replaceAll(webpage, '!%SHOWJOIN%!', showjoin);
-					webpage = common.replaceAll(webpage, '!%SHOWUNJOIN%!', showunjoin);
-					webpage = common.replaceAll(webpage, '!%SHOWATTEND%!', showattend);
-					webpage = common.replaceAll(webpage, '!%SHOWUNATTEND%!', showunattend);
-					webpage = common.replaceAll(webpage, '!%SHOWRESET%!', showreset);
-					
-					webpage = webpage.replace('!%SHOWPOST%!', showpost);
-					webpage = webpage.replace('!%SHOWINVITE%!', showinvite);
-					
-					res.cookie('activity' , actionlink);
-					
-					var whosgoingsql = "SELECT meetspace.user.username, meetspace.whosgoing.status FROM meetspace.whosgoing JOIN meetspace.user ON meetspace.whosgoing.userId = meetspace.user.id WHERE meetspace.whosgoing.activityId = " + activityId;
-					
+					var activityId = result.rows[0].ret_activityid;					
+					var title = result.rows[0].ret_title;
+					var time = result.rows[0].ret_time;
+					var day = result.rows[0].ret_day;
+					game = result.rows[0].ret_game;
+					description = result.rows[0].ret_description;
+				
+					sql = "SELECT * FROM meetspace.get_activity_details($1, $2, $3);"
+				
 					pool.connect(function(err, client, done) {
-						client.query(whosgoingsql , function(err, result) {
+						if (err) {
+							console.log("ERROR: " + err);
+						}
+						
+						client.query(sql, [email, sessionId, activityId], function(err, result) {
 							done();
 							
-							var whosgoing = [];
-							var whosnot = [];
-							
-							if (!result) {
-								whosnot.push('no body');
+							if (err) {
+								console.log("error fail");
 							} else {
-								for (var i = 0; i < result.rows.length; i++) {
-									var username = result.rows[i].username;
-									var status = result.rows[i].status;
-									
-									if (status == 1) {
-										whosgoing.push(username);
-									} else {
-										whosnot.push(username);
-									}
+								if (username && sessionId) {
+									showjoin = 'inline';
+									showunjoin = 'inline';
+									showattend = 'inline';
+									showunattend = 'inline';
+									showreset = 'inline';
+									showpost = 'inline';
+									showinvite = 'inline';
 								}
 								
-								webpage = renderElement.whosgoing(webpage, whosgoing, whosnot);
-							}
-
-							var postsql = "SELECT username, message, postdate FROM meetspace.post INNER JOIN meetspace.user ON meetspace.post.userid = meetspace.user.id WHERE activityid = $1 ORDER BY postdate DESC;";
-							
-							pool.connect(function(err, client, done) {
-								client.query(postsql, [activityId], function(err, result) {
-									done();
-							
-									var postdates  = [];
-									var postusernames  = [];
-									var postmessages  = [];
-							
-									if(result) {
-										for (var i = 0; i < result.rows.length; i++) {
-											var postusername = result.rows[i].username;
-											var postmessage = result.rows[i].message;
-											var postdate = result.rows[i].postdate;
+								webpage = renderElement.activityTitle(webpage, title);
+								webpage = renderElement.activityTime(webpage, day, time);
+								webpage = webpage.replace('!%DESCRIPTION%!', description);
+								webpage = common.replaceAll(webpage, '!%ACTION%!', actionlink);
+								webpage = common.replaceAll(webpage, '!%ACTIVITYID%!', activityId);
+													
+								webpage = common.replaceAll(webpage, '!%SHOWJOIN%!', showjoin);
+								webpage = common.replaceAll(webpage, '!%SHOWUNJOIN%!', showunjoin);
+								webpage = common.replaceAll(webpage, '!%SHOWATTEND%!', showattend);
+								webpage = common.replaceAll(webpage, '!%SHOWUNATTEND%!', showunattend);
+								webpage = common.replaceAll(webpage, '!%SHOWRESET%!', showreset);
+								
+								webpage = webpage.replace('!%SHOWPOST%!', showpost);
+								webpage = webpage.replace('!%SHOWINVITE%!', showinvite);
+								
+								res.cookie('activity' , actionlink);
+								
+								var whosgoingsql = "SELECT meetspace.user.username, meetspace.whosgoing.status FROM meetspace.whosgoing JOIN meetspace.user ON meetspace.whosgoing.userId = meetspace.user.id WHERE meetspace.whosgoing.activityId = " + activityId;
+								
+								pool.connect(function(err, client, done) {
+									client.query(whosgoingsql , function(err, result) {
+										done();
+										
+										var whosgoing = [];
+										var whosnot = [];
+										
+										if (!result) {
+											whosnot.push('no body');
+										} else {
+											for (var i = 0; i < result.rows.length; i++) {
+												var username = result.rows[i].username;
+												var status = result.rows[i].status;
+												
+												if (status == 1) {
+													whosgoing.push(username);
+												} else {
+													whosnot.push(username);
+												}
+											}
 											
-											postdates.push(postdate);
-											postusernames.push(postusername);
-											postmessages.push(postmessage);
+											webpage = renderElement.whosgoing(webpage, whosgoing, whosnot);
 										}
-									}
-							
-									webpage = renderElement.posts(webpage, country, region, postdates, postusernames, postmessages);
-							
-									res.send(webpage);
+
+										var postsql = "SELECT username, message, postdate FROM meetspace.post INNER JOIN meetspace.user ON meetspace.post.userid = meetspace.user.id WHERE activityid = $1 ORDER BY postdate DESC;";
+										
+										pool.connect(function(err, client, done) {
+											client.query(postsql, [activityId], function(err, result) {
+												done();
+										
+												var postdates  = [];
+												var postusernames  = [];
+												var postmessages  = [];
+										
+												if(result) {
+													for (var i = 0; i < result.rows.length; i++) {
+														var postusername = result.rows[i].username;
+														var postmessage = result.rows[i].message;
+														var postdate = result.rows[i].postdate;
+														
+														postdates.push(postdate);
+														postusernames.push(postusername);
+														postmessages.push(postmessage);
+													}
+												}
+										
+												webpage = renderElement.posts(webpage, country, region, postdates, postusernames, postmessages);
+										
+												res.send(webpage);
+											});
+										});
+									});
 								});
-							});
+							}
 						});
 					});
 				} else if (result.rows.length == 0) {
