6d83093284670e19d44c41f18e4271b0ef74e14c
oelderinkX <joelderink.wale@gmail.com>
2017-03-14 21:06:56 +1300

    startYouTube4

--- script/renderElement.js
+++ script/renderElement.js
@@ -86,7 +86,7 @@ function posts(webpage, country, region, postdates, postusernames, postmessages)
 			youtubeEmedded = youtubeEmedded.replace('watch?v=' ,'embed/');
 			
 			
-			youtubeEmedded = '<div class="embed-responsive embed-responsive-16by9"><iframe class="col-sm-6" frameborder="0" src="' + youtubeEmedded;
+			youtubeEmedded = '<div class="embed-responsive embed-responsive-16by9"><iframe class="col-sm-6" frameborder="0" allowfullscreen src="' + youtubeEmedded;
 			youtubeEmedded += '"></iframe></div>';
 			
 			postmessage = postmessage.substring(0,youtubeLinkStart) + youtubeEmedded + postmessage.substring(youtubeLinkEnd);
