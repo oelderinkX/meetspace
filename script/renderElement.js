feb8829e83ab0c1a679a0e53e9cbdf5cd073b62e
oelderinkX <joelderink.wale@gmail.com>
2017-03-14 20:53:07 +1300

    startYouTube4

--- script/renderElement.js
+++ script/renderElement.js
@@ -78,7 +78,7 @@ function posts(webpage, country, region, postdates, postusernames, postmessages)
 				youtubeLinkEnd = postmessage.length;
 			}
 			
-			var youtubeEmedded = '<div class="embed-container"><iframe class="col-sm-6" frameborder="0" src="';
+			var youtubeEmedded = '<div class="embed-responsive embed-responsive-16by9"><iframe class="col-sm-6" frameborder="0" src="';
 			youtubeEmedded += postmessage.substr(youtubeLinkStart, youtubeLinkEnd);
 			youtubeEmedded += '"></iframe></div>';
 			youtubeEmedded = youtubeEmedded.replace('watch?v=' ,'embed/');
