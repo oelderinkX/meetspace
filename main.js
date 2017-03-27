916c6757ffcec1a56579c79dec40087017556793
oelderinkX <joelderink.wale@gmail.com>
2017-03-27 20:20:13 +1300

    main

--- main.js
+++ main.js
@@ -4,13 +4,15 @@ var fs = require("fs");
 const util = require('util');
 var common = require('./script/common.js');
 
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
 var pool = new pg.Pool(common.postgresConfig());
 
 var mainPage = fs.readFileSync(__dirname + "/webpage/main.html", "utf8");
 
 module.exports = function(app){
 	
-	app.get('/', function(req, res) {
+	app.get('/', urlencodedParser, function(req, res) {
 		var showjoin = 'inline';
 		var username = req.cookies['username'];
 		var sessionId = req.cookies['sessionId'];
