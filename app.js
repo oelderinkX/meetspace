754fbea4844efbea91508c829d94e22733e53ce3
oelderink <joelderink.wale@gmail.com>
2019-01-14 20:42:44 +1300

    cronit

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ var express = require('express');
 var app = express();
 var favicon = require('serve-favicon');
 var cookieParser = require('cookie-parser')
+var cron = require('node-schedule');
 var port = process.env.PORT || 80
 
 console.log('port number is: ' + port);
+
+var j = cron.scheduleJob('*/1 * * * *', function(fireDate){
+  console.log('This job was supposed to run at ' + fireDate + ', but actually ran at ' + new Date());
+});
 
 app.use(favicon(__dirname + '/favicon.ico'));
 
