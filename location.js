9b1a7dc66ab87019037ff0e3ad3803a128571ed4
oelderink <joelderink.wale@gmail.com>
2019-02-23 19:13:42 +1300

    locations

--- location.js
+++ location.js
@@ -8,6 +8,8 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false });
 var pool = new pg.Pool(common.postgresConfig());
 
 var countries = [];
+var regions = [];
+var cities = [];
 
 module.exports = function(app) {
 	app.get('/getcountries', function(req, res) {
@@ -30,6 +32,58 @@ module.exports = function(app) {
 						}
 					}
 
+					res.send(countries);
+				});
+			});
+		}
+	});	
+}
+
+module.exports = function(app) {
+	app.get('/regionsByCountryId', function(req, res) {
+		var country_id = req.query.id;
+		var regionByCountry = [];
+		
+		if (regions.length > 0) {
+			for (var i = 0; i < regions.length ; i++) {
+				if (regions[i].country_id == country_id) {
+					regionByCountry.push({
+						id: regions[i].id,
+						name: regions[i].name,
+						code: regions[i].code,
+						country_id: regions[i].country_id
+					});
+				}
+			}			
+			res.send(regionByCountry);
+		} else {
+			var postsql = "select id, name, code, country_id from meetspace.regions;";
+			pool.connect(function(err, client, done) {
+				client.query(postsql, function(err, result) {
+					done();
+
+					if (result) {
+						for (var i = 0; i < result.rows.length; i++) {
+							regions.push({
+							  id: result.rows[i].id,
+							  name: result.rows[i].name,
+							  code: result.rows[i].code
+							  code: result.rows[i].country_id
+							});
+						}
+					}
+
+					for (var i = 0; i < regions.length ; i++) {
+						if (regions[i].country_id == country_id) {
+							regionByCountry.push({
+								id: regions[i].id,
+								name: regions[i].name,
+								code: regions[i].code,
+								country_id: regions[i].country_id
+							});
+						}
+					}
+					
 					res.send(countries);
 				});
 			});
