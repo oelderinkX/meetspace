59886ac159cdafe3e2e16400eca49a7813dd7223
oelderink <joelderink.wale@gmail.com>
2019-02-23 18:46:46 +1300

    locations

--- location.js
+++ location.js
@@ -7,28 +7,32 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 var pool = new pg.Pool(common.postgresConfig());
 
+var countries = [];
+
 module.exports = function(app){
 	app.get('/getcountries', function(req, res) {
-		
-		var postsql = "select id, name, code from meetspace.countries;";
-		pool.connect(function(err, client, done) {
-			client.query(postsql, function(err, result) {
-				done();
-				
-				var countries = [];
 
-				if (result) {
-					for (var i = 0; i < result.rows.length; i++) {
-						countries.push({
-						  id: result.rows[i].id,
-						  name: result.rows[i].name,
-						  code: result.rows[i].code,
-						});
+		if (countries) {
+			res.send(countries);
+		} else {
+			var postsql = "select id, name, code from meetspace.countries;";
+			pool.connect(function(err, client, done) {
+				client.query(postsql, function(err, result) {
+					done();
+
+					if (result) {
+						for (var i = 0; i < result.rows.length; i++) {
+							countries.push({
+							  id: result.rows[i].id,
+							  name: result.rows[i].name,
+							  code: result.rows[i].code,
+							});
+						}
 					}
-				}
 
-				res.send(countries);
+					res.send(countries);
+				});
 			});
-		});
-	});	
+		});	
+	}
 }
