407ae8e9614abbaea4602fd147d282395a2ea182
oelderink <joelderink.wale@gmail.com>
2020-11-14 14:57:24 +1300

    make sure PM is adding 12 hours

--- newactivity.js
+++ newactivity.js
@@ -33,7 +33,7 @@ module.exports = function(app){
 		var country = req.body.country;
 		var description = req.body.activitydescription;
 		var hour = req.body.hour;
-		if (req.body.ampm == "")
+		if (req.body.ampm == "PM")
 		{
 			hour = hour + 12;
 		}
