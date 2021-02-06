ffed1fea962737ac94a23f937ed422a768fb7f11
oelderink <joelderink.wale@gmail.com>
2021-02-06 16:19:58 +1300

    move to p

--- script/renderElement.js
+++ script/renderElement.js
@@ -135,7 +135,7 @@ function posts(webpage, country, region, posts) {
 		var adjustedDateTime = getGmtAdjustedDateTime(post.submissionDate, country, region);
 		var agoTxt = getAgo(adjustedDateTime);
 		
-		postElement += ' <div class="alert-message alert-message-default text-break"><strong>' + post.username + '</strong> ' + agoTxt + '<p>' + post.message + '</p></div>';
+		postElement += ' <div class="alert-message alert-message-default"><strong>' + post.username + '</strong> ' + agoTxt + '<p class="text-break">' + post.message + '</p></div>';
 	});
 	
 	webpage = webpage.replace('!%POSTS%!', postElement);
