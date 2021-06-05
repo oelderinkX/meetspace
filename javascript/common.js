fd23fd917e372e3c801c57e6822153fffe0bf253
oelderink <joelderink.wale@gmail.com>
2021-06-05 16:22:06 +1200

    big change, make global things

--- javascript/common.js
+++ javascript/common.js
@@ -1,36 +1,44 @@
 function setCookie(cname, cvalue) {
-    var timeElapsed  = Date().now;
-    var d = new Date(timeElapsed);
-    d.setTime(d.getDate() + 365);
-    var expires = "expires="+ d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-  }
+  var timeElapsed  = Date().now;
+  var d = new Date(timeElapsed);
+  d.setTime(d.getDate() + 365);
+  var expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
   
-  function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-      var c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
     }
-    return "";
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
   }
+  return "";
+}
 
-  function sendPost(url, data, callback) {
-    var xhttp = new XMLHttpRequest();
-    xhttp.open("POST", url, true);
-    xhttp.setRequestHeader("Content-type", "application/json");
-    xhttp.send(data);
-  
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        callback(this.response);
-      }
-    };
-  }
+function sendPost(url, data, callback) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.open("POST", url, true);
+  xhttp.setRequestHeader("Content-type", "application/json");
+  xhttp.send(data);
+
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      callback(this.response);
+    }
+  };
+}
+
+function hide(element) {
+  element.style = "display: none";
+}
+
+function show(element) {
+  element.style = "display: inline";
+}
