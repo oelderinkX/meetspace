014e37bc4d96568325bbbdd6e2a3a202ba114fe2
Jared <jared.oelderinkwale@gmail.com>
2025-11-12 21:00:29 +1300

    remove log variable

--- logging.js
+++ logging.js
@@ -1,11 +1,10 @@
 function logDbStats(msg, pool) {
-    if (log) {
-        if (pool) {
-            console.log(msg);
-            console.log(`pg.Pool.totalCount: ${pool.totalCount}`);
-            console.log(`pg.Pool.idleCount: ${pool.idleCount}`);
-            console.log(`pg.Pool.waitingCount: ${pool.waitingCount}`);
-        }
+    if (pool) {
+        console.log(msg);
+        console.log(`pg.Pool.totalCount: ${pool.totalCount}`);
+        console.log(`pg.Pool.idleCount: ${pool.idleCount}`);
+        console.log(`pg.Pool.waitingCount: ${pool.waitingCount}`);
     }
+
 }
 module.exports.logDbStats = logDbStats;
